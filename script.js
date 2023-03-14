f01603ba385dc4aa0badaa37a6834cd823842b61
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-03-15 03:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var yourArray=['one', 2, 'three', true, false];
-console.log(yourArray);
+var yourArray=["one", 2, true, undefined, null];
+for (let index = 0; index < yourArray.length; index++) {
+	console.log(yourArray[index]);
+}
